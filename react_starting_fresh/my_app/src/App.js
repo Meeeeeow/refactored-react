e27dca688e6339bc62c50498a95dec2e55bdae23
Meeeeeow <kazutokun467@gmail.com>
2022-03-08 11:58:31 +0600

    Hoc pattern

--- react_starting_fresh/my_app/src/App.js
+++ react_starting_fresh/my_app/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Clock from './Component/clock/clock';
 import Form from './Component/form/form.component';
 import Calculator from './Component/temperature/calculator';
+import ClickCounter from './Component/hoc/clickCounter';
+import HoverCounter from './Component/hoc/hoverCounter';
 function App() {
   return (
     <div className="App">
       <Clock>I am children</Clock>
       <Form/>
       <Calculator/>
+      <ClickCounter/>
+      <HoverCounter/>
     </div>
   );
 }
